049a8592aa80f37a55d97de6f7174dcfa7637f09
jasoliyajishan <132878572+jasoliyajishan@users.noreply.github.com>
2023-05-19 13:36:45 +0530

    Add files via upload

---
+++ m4139.2_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+ {
+    int i=10 ;
+
+    for(i;i >= 1;i--)
+    {
+    	  printf("%d\n", i);
+	}
+
+    return 0;
+}
---
+++ m4139.2_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+ {
+    int i=1,n;
+    printf("enter a number");
+    scanf("%d",&n);
+
+   for(i;i <= n; i++)
+   {
+   
+    printf("%d\n", i);
+    }
+
+    return 0;
+}
